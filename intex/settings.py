a505649baa7de9f90a69c707f2822b20a14c5c93
ulugbek <ulugbek9015t@gmail.com>
2022-04-05 08:06:40 +0500

    sA

--- intex/settings.py
+++ intex/settings.py
@@ -152,7 +152,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-AUTH_USER_MODEL = 'basic_app.CustomUser'
+
+# AUTH_USER_MODEL = 'basic_app.CustomUser'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 #dsa
